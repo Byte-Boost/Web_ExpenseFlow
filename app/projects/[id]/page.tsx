61d85e186331596ca6a5faa8c9c8e9ec468c8590
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2025-05-04 19:19:11 -0300

    [feat]: option to add/remove quantityValues

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -7,6 +7,7 @@ import { useParams } from 'next/navigation';
 export default function Home() {
     const params = useParams();
     const projectId = Number(params.id);
+    const [addingQuantity, setAddingQuantity] = useState(false);
     const [project, setProject] = useState<Project|null>(null);
     const [formData, setFormData] = useState<ProjectPreference|null>({
         refundLimit: 0,
@@ -29,7 +30,17 @@ export default function Home() {
             quantityValues: updated
         }))
     }
-
+    const removeQuantityValue = (key:any) =>{
+        const updated = [...(formData?.quantityValues || [])]
+        const index = updated.findIndex((value: any) => Object.keys(value)[0] === key)
+        if (index !== -1) {
+            updated.splice(index, 1)
+            setFormData((prev: any) => ({
+                ...prev,
+                quantityValues: updated
+            }))
+        }
+    }
     async function updateProjectPreferences(preference: ProjectPreference) {
         const response: any = await updatePreference(projectId, preference);
         return response;
@@ -52,61 +63,98 @@ export default function Home() {
                 <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
                     #{projectId} - {project?.name}
                 </h1>
-                <form className="mt-4 space-y-4 w-full max-w-xl" onSubmit={(e) => {
-                    e.preventDefault()
-                    updateProjectPreferences(formData as ProjectPreference).then((response) => {
-                        console.log(response)
-                    })
-                }}>
+                <form className="mt-4 space-y-4 w-full max-w-xl" 
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') e.preventDefault()
+                    }}
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        updateProjectPreferences(formData as ProjectPreference).then((response) => {
+                            console.log(response)
+                        })
+                    }}
+                >
                     <div>
-                    <label className="block font-medium text-gray-700 dark:text-gray-200">Refund Limit</label>
-                    <input
-                        name="refundLimit"
-                        type="number"
-                        step="0.01"
-                        value={formData?.refundLimit}
-                        onChange={handleInputChange}
-                        className="w-full mt-1 p-2 rounded border text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-800"
-                    />
-                    </div>
-
-                    <div>
-                    <label className="block font-medium text-gray-700 dark:text-gray-200">Expense Limit</label>
-                    <input
-                        name="expenseLimit"
-                        type="number"
-                        step="0.01"
-                        value={formData?.expenseLimit}
-                        onChange={handleInputChange}
-                        className="w-full mt-1 p-2 rounded border text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-800"
-                    />
-                    </div>
-
-                    <div>
-                    <label className="block font-medium text-gray-700 dark:text-gray-200">Quantity Values</label>
-                    {formData?.quantityValues.map((value, index) => {
-                        const key = Object.keys(value)[0]
-                        return (
-                        <div key={index} className="flex justify-between items-center gap-2 mt-1 text-gray-700 dark:text-gray-200 ">
-                            <span className="text-sm font-semibold">{key}</span>
-                            <input
+                        <label className="block font-medium text-gray-700 dark:text-gray-200">Refund Limit</label>
+                        <input
+                            name="refundLimit"
                             type="number"
                             step="0.01"
-                            value={value[key]}
-                            onChange={(e) => handleQuantityValueChange(index, key, e.target.value)}
-                            className="max-w-3xl p-2 rounded border bg-white dark:bg-gray-800"
-                            />
-                        </div>
-                        )
-                    })}
+                            value={formData?.refundLimit}
+                            onChange={handleInputChange}
+                            className="w-full mt-1 p-2 rounded border text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-800"
+                        />
                     </div>
 
-                    <button
-                    type="submit"
-                    className="mt-4 w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 cursor-pointer"
-                    >
-                        Save Preferences
-                    </button>
+                    <div>
+                        <label className="block font-medium text-gray-700 dark:text-gray-200">Expense Limit</label>
+                        <input
+                            name="expenseLimit"
+                            type="number"
+                            step="0.01"
+                            value={formData?.expenseLimit}
+                            onChange={handleInputChange}
+                            className="w-full mt-1 p-2 rounded border text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-800"
+                        />
+                    </div>
+
+                    <div>
+                        <label className="block font-medium text-gray-700 dark:text-gray-200">Quantity Values</label>
+                        {formData?.quantityValues.map((value, index) => {
+                            const key = Object.keys(value)[0]
+                            return (
+                            <div key={index} className="flex justify-between items-center gap-2 mt-1 text-gray-700 dark:text-gray-200 ">
+                                <span>
+                                    <button type="button" className="font-bold bg-red-600 hover:bg-red-700 py-0 px-4 text-white rounded cursor-pointer" onClick={()=>{
+                                        removeQuantityValue(key);
+                                    }}>
+                                        -
+                                    </button>
+                                    <span className="ml-2 text-sm font-semibold">{key}</span>
+                                </span>
+                                <input
+                                type="number"
+                                step="0.01"
+                                value={value[key]}
+                                onChange={(e) => handleQuantityValueChange(index, key, e.target.value)}
+                                className="max-w-3xl p-2 rounded border bg-white dark:bg-gray-800"
+                                />
+                            </div>
+                            )
+                        })}
+
+                        { addingQuantity && (
+                            <div className="flex justify-between items-center gap-2 mt-1 text-gray-700 dark:text-gray-200">
+                                <input className="text-sm font-semibold p-2 rounded border bg-white dark:bg-gray-800" type="text" onKeyDown={(e) => {
+                                    if (e.key === 'Enter') {
+                                        e.preventDefault()
+                                        const newKey = e.currentTarget.value
+
+                                        if (newKey && !formData?.quantityValues.some((value: any) => Object.keys(value)[0] === newKey)) {
+                                            setFormData((prev: any) => ({
+                                                ...prev,
+                                                quantityValues: [...(prev.quantityValues || []), {[newKey]: 0}]
+                                            }));
+                                            e.currentTarget.value = '';
+                                            setAddingQuantity(false);
+                                        }
+                                    }
+                                }}/>
+                            </div>
+                        )}
+                    </div>
+
+                    <section className="flex gap-3 items-center justify-center mt-4">
+                        <button type="button" className="mt-4 bg-green-600 text-white py-2 px-4 rounded hover:bg-green-700 cursor-pointer" onClick={()=>{
+                            setAddingQuantity(true);
+                        }}>
+                            +
+                        </button>
+
+                        <button type="submit" className="mt-4 w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 cursor-pointer">
+                            Save Preferences
+                        </button>
+                    </section>
                 </form>
             </section>
         </div>
