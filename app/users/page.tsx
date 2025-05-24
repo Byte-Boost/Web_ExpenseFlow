d1a41a6b9657001e6242e112d178fc46ca21a78a
Vinicius Forcato <76211125+nininhosam@users.noreply.github.com>
2025-05-24 16:08:31 -0300

    [feat]: forms to create users

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,7 +2,7 @@
 import { NavBar } from "@/app/modules/navbar/index";
 import { useEffect, useState } from "react";
 import { confirmationAlert, failureAlert, successAlert } from "../utils/alerts";
-import { getProjects, getUsers, setSubscriptions } from "../utils/endpoints";
+import { getProjects, getUsers, registerUser, setSubscriptions } from "../utils/endpoints";
 import { HiOutlinePencilAlt } from "react-icons/hi";
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader, ToggleSwitch } from "flowbite-react";
 
@@ -13,21 +13,24 @@ export default function Home() {
   const [selectedProjects, setSelectedProjects] = useState<any>([]);
   const [selectedUser, setSelectedUser] = useState<any>(null);
   
+  const [newUserEmail, setNewUserEmail] = useState("");
+  const [newUserPassword, setNewUserPassword] = useState("");
+
   async function handleProjectToggle(projId: number) {
     if (selectedProjects.includes(projId)) 
       setSelectedProjects(selectedProjects.filter((id: number) => id !== projId));
     else 
       setSelectedProjects([...selectedProjects, projId]);
-  }
+  };
 
   async function setUserProjects(userId: number, projectIds: number[]) {
     return await setSubscriptions(userId, projectIds);
-  }
+  };
 
   const fetchProjects = async () =>{
     const response = await getProjects();
     setProjects(response);
-  }
+  };
 
   const fetchUsers = async () => {
     await fetchProjects();
@@ -35,6 +38,12 @@ export default function Home() {
     setIsLoading(false);
     setUsers(response);
     setSelectedUser(null);
+  };
+
+  async function createNewUser(email: string, password: string) {
+    let res = await registerUser(email, password);
+    fetchUsers();
+    return res;
   };
 
   useEffect(() => {
@@ -48,14 +57,70 @@ export default function Home() {
     } else {
       setSelectedProjects([]);
     }
-  }, [selectedUser])
+  }, [selectedUser]);
 
   return (
-    <main className="flex min-h-screen flex-col bg-gray-100 dark:bg-gray-900">
+    <main className="min-h-screen flex flex-col bg-gray-100 dark:bg-gray-900">
       <NavBar></NavBar>
-      
-      <div className="overflow-auto max-h-[600px] p-4">
-        <table className="w-full text-left border-collapse text-black dark:text-gray-400 table-fixed">
+
+      <form className="m-4 w-full max-w-xl space-y-4 self-center"
+        onKeyDown={(e) => {
+          if (e.key === "Enter") e.preventDefault();
+        }}
+        onSubmit={(e) => {
+          e.preventDefault();
+          createNewUser(newUserEmail, newUserPassword)
+          .then(response=>{
+            successAlert("Usuário criado com sucesso", "user created successfully");
+            setNewUserEmail("");
+            setNewUserPassword("");
+          })
+          .catch(err=>{
+            console.log(err);
+            failureAlert("Erro ao criar usuário", "something went wrong while creating user");
+          });
+        }}
+      >
+        <div>
+          <label className="block font-medium text-gray-700 dark:text-gray-200" htmlFor="emailInput">Email</label>
+          <div className="mt-1 flex w-full rounded border bg-white text-gray-700 focus-within:ring-2 focus-within:ring-gray-400 dark:bg-gray-800 dark:text-gray-200">
+            <input
+              name="emailInput"
+              type="text"
+              value={newUserEmail}
+              onChange={(e)=>{setNewUserEmail(e.target.value)}}
+              className="grow border-none bg-transparent p-2 outline-none"
+            />
+          </div>
+        </div>
+
+        <div>
+          <label className="block font-medium text-gray-700 dark:text-gray-200" htmlFor="passwordInput">
+            Senha
+          </label>
+          <div className="mt-1 flex w-full rounded border bg-white text-gray-700 focus-within:ring-2 focus-within:ring-gray-400 dark:bg-gray-800 dark:text-gray-200">
+            <input
+              name="passwordInput"
+              type="password"
+              value={newUserPassword}
+              onChange={(e)=>{setNewUserPassword(e.target.value)}}
+              className="grow border-none bg-transparent p-2 outline-none"
+            />
+          </div>
+        </div>
+
+        <section className="mt-4 flex items-center justify-center gap-3">
+          <button
+            type="submit"
+            className="mt-4 w-full cursor-pointer rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
+          >
+            Criar usuário
+          </button>
+        </section>
+      </form>
+
+      <div className="overflow-auto max-h-[70vmin] p-4 fancyScroll">
+        <table className="w-full text-left border-collapse text-black dark:text-gray-400">
           <thead>
             <tr>
               <th className="px-4 py-2">#</th>
@@ -84,10 +149,10 @@ export default function Home() {
 
       {selectedUser && (
         <Modal className="text-black dark:text-gray-400" show={true} onClose={() => setSelectedUser(null)} dismissible>
-          <ModalHeader theme={{close: {base: 'text-black dark:text-gray-400 cursor-pointer',}}}>
+          <ModalHeader theme={{close: {base: 'text-black dark:text-gray-400 cursor-pointer'}}}>
             Editar Projetos do Usuário
           </ModalHeader>
-          <ModalBody className="fancyScroll">
+          <ModalBody className="fancyScroll max-h-[48vh]">
             <div className="mb-4">
               <p className="font-semibold">Email:</p>
               <p>{selectedUser.email}</p>
